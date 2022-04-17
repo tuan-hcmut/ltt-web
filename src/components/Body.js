54e3784bcb3ee7ca0049db6903c3312287537069
tuan <tuan01677239677@gmail.com>
2022-04-17 11:37:30 +0700

    day4

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const radio = ["radio1", "radio2", "radio3"];
 
 function SlideShow() {
+  const [reSize, setReSize] = useState(window.screenX);
+  const [fillDot, setFillDot] = useState("radio1");
+
+  const handleFillDot = (val) => {
+    setFillDot();
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setReSize(window.innerWidth);
+    };
+    window.addEventListener("resize", handleScroll);
+  }, []);
+
   return (
     <div className="slider-containner">
       <div className="slides">
-        <input type={"radio"} name="radio-btn" id="radio1" />
-        <input type={"radio"} name="radio-btn" id="radio2" />
-        <input type={"radio"} name="radio-btn" id="radio3" />
-        <input type={"radio"} name="radio-btn" id="radio4" />
+        {radio.map((radio) => {
+          return (
+            <input type={"radio"} name="radio-btn" id={radio} key={radio} />
+          );
+        })}
 
-        <div className="slide">
-          <div
-            className="slide__img1"
-            role={"img"}
-            aria-label="one-peace"
-          ></div>
-
-          <div className="slide__img2" role={"img"} aria-label="caro"></div>
-
-          <div className="slide__img3" role={"img"} aria-label="chess"></div>
+        <div className="slide first">
+          <img src="one-peace.jpg" alt="" style={{ width: reSize }} />
         </div>
+        <div className="slide">
+          <img src="caro.jpg" alt="" style={{ width: reSize }} />
+        </div>
+        <div className="slide">
+          <img src="chess.jpg" alt="" style={{ width: reSize }} />
+        </div>
+      </div>
+      <div className="slide-manual">
+        {/* lable its will link to the its input so when you click it will checked this input */}
+
+        {radio.map((radio) => {
+          let color = radio === fillDot ? "#2568ef" : "transparent";
+          return (
+            <label
+              htmlFor={radio}
+              className="manual-btn"
+              key={radio}
+              style={{ backgroundColor: color }}
+              // onChange={setFillDot(radio)}
+            ></label>
+          );
+        })}
       </div>
     </div>
   );
